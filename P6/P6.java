3aea54ccb82fca4991e94343022ad4995b0b7e26
mikhil-c <mikhil-c@users.noreply.github.com>
2025-11-03 21:50:21 +0530

    P6: Initial commit

---
+++ P6/P6.java
@@ -0,0 +1,15 @@
+import syntaxtree.*;
+import visitor.*;
+
+public class P6 {
+   public static void main(String[] args) {
+      try {
+         Node root = new MiniRAParser(System.in).Goal();
+      }
+      catch (ParseException e) {
+         System.out.println(e.toString());
+      }
+   }
+} 
+
+
